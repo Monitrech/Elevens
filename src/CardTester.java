eb3f9e1e05b8fef46cf2d78cc24f29681105e29a
Adam Fowler <monitrech@gmail.com>
2016-03-31 14:53:15 -0500

    Finished Activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,7 +8,37 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("test1", "hears", 5);
+		Card card2 = new Card("test2", "diamonds", 7);
+		Card card3 = new Card("test3", "spades", 9);
+		
+		System.out.println(card1.suit());
+		System.out.println(card1.rank());
+		System.out.println(card1.pointValue());
+		
+		System.out.println(card2.suit());
+		System.out.println(card2.rank());
+		System.out.println(card2.pointValue());
+		
+		System.out.println(card3.suit());
+		System.out.println(card3.rank());
+		System.out.println(card3.pointValue());
+		
+		
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+		System.out.println(card3.toString());
+		
+		
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card1));
+		
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.matches(card2));
+		
+		System.out.println(card1.matches(card3));
+		System.out.println(card3.matches(card1));
+		
 	}
 }
 
